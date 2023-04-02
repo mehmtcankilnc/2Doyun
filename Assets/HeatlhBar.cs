50a925a3388bbc05c3c02357ae44cc31d96cb2ce
Mehmetcan Kılınç <mehmtcankilinc@hotmail.com>
2023-04-02 23:47:17 +0300

    1

---
+++ Assets/HeatlhBar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeatlhBar : MonoBehaviour
+{
+    public Slider slider;
+    public void SetHealth(int health){
+        slider.value = health;
+    }
+}
