8c267b3fde6b319bb02f14427305a625e2b92d27
Mehmetcan Kılınç <mehmtcankilinc@hotmail.com>
2023-04-05 21:24:21 +0300

    1

--- Assets/Scripts/Player/ItemCollector.cs
+++ Assets/Scripts/Player/ItemCollector.cs
@@ -13,18 +13,29 @@ public class ItemCollector : MonoBehaviour
     private int gems = 0;
     [SerializeField]private AudioSource carrotSound;
     [SerializeField]private AudioSource gemSound;
+    void Start(){
+        int collectedCarrot = PlayerPrefs.GetInt("CollectedCarrot");
+        int collectedGem = PlayerPrefs.GetInt("CollectedGem");
+        carrotsText.text = "Carrots : "+ PlayerPrefs.GetInt("CollectedCarrot");
+        gemsText.text = "Gems : " + PlayerPrefs.GetInt("CollectedGem");
+    }
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.CompareTag("Carrot")){
+            carrots = PlayerPrefs.GetInt("CollectedCarrot");
             carrotSound.Play();
             Destroy(collision.gameObject);
             carrots++;
             carrotsText.text = "Carrots : "+ carrots;
+            PlayerPrefs.SetInt("CollectedCarrot", carrots);
+            
         }
         else if(collision.gameObject.CompareTag("Gem")){
+            gems = PlayerPrefs.GetInt("CollectedGem");
             gemSound.Play();
             Destroy(collision.gameObject);
             gems++;
             gemsText.text = "Gems : "+ gems;
+            PlayerPrefs.SetInt("CollectedCarrot", gems);
         }
     }
 }
